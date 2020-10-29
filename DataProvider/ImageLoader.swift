478818819e2ab7cdcbbecb3306d8300e86ed76c0
sergd2005 <sergd2005@gmail.com>
2020-10-28 20:40:59 -0700

    removing comments

--- DataProvider/ImageLoader.swift
+++ DataProvider/ImageLoader.swift
@@ -12,31 +12,24 @@ class ImageLoader {
   private var runningRequests = [UUID: URLSessionDataTask]()
     
     func loadImage(_ url: URL, _ completion: @escaping (Result<UIImage, Error>) -> Void) -> UUID? {
-
-      // 1
       if let image = loadedImages[url] {
         completion(.success(image))
         return nil
       }
 
-      // 2
       let uuid = UUID()
 
       let task = URLSession.shared.dataTask(with: url) { data, response, error in
-        // 3
+        
         defer {self.runningRequests.removeValue(forKey: uuid) }
 
-        // 4
         if let data = data, let image = UIImage(data: data) {
           self.loadedImages[url] = image
           completion(.success(image))
           return
         }
 
-        // 5
         guard let error = error else {
-          // without an image or an error, we'll just ignore this for now
-          // you could add your own special error cases for this scenario
           return
         }
 
@@ -49,7 +42,6 @@ class ImageLoader {
       }
       task.resume()
 
-      // 6
       runningRequests[uuid] = task
       return uuid
     }
